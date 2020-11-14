6d74b2d236adbaf5c30811171136035f308025aa
tarun2008 <70888164+tarun2008@users.noreply.github.com>
2020-11-14 10:07:37 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Multiplayer-Car-Racing-1
+# asynchronousBallMovement
+Boiler plate for ball moving aysnchronously
---
+++ sketch.js
@@ -0,0 +1,16 @@
+var backgroundImg;
+var gameState = 0;
+var playerCount;
+var database;
+var form,player,game;
+
+function setup(){
+    database= firebase.database();
+    createCanvas(400,400);
+    
+}
+
+function draw(){
+   
+}
+
